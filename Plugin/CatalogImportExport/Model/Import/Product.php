27d06863c5238597abfa8a260e55c561dfa6e6b6
Renon Stewart <srenon@users.noreply.github.com>
2019-06-25 12:13:07 -0400

    Issues 5 - Incomplete namespace

--- Plugin/CatalogImportExport/Model/Import/Product.php
+++ Plugin/CatalogImportExport/Model/Import/Product.php
@@ -2,11 +2,11 @@
 
 namespace MagePal\LinkProduct\Plugin\CatalogImportExport\Model\Import;
 
-use Magento\CatalogImportExport\Model\Import\Produc;
+use Magento\CatalogImportExport\Model\Import\Product;
 use MagePal\LinkProduct\Model\Product\Link;
 
 /**
- * @see \Magento\CatalogImportExport\Model\Import\Product::getLinkNameToId
+ * @see Product::getLinkNameToId
  */
 class Product
 {
@@ -14,11 +14,11 @@ class Product
      * REMARK: needs core patch
      * https://github.com/magento/magento2/pull/21230/commits/0846e9aed7040659e7ce3e109eb91df3f5fdfb7e.patch
      *
-     * @param \Magento\CatalogImportExport\Model\Import\Product $subject
+     * @param Product $subject
      * @param $result
      * @return mixed
      */
-    public function afterGetLinkNameToId(\Magento\CatalogImportExport\Model\Import\Product $subject, $result)
+    public function afterGetLinkNameToId(Product $subject, $result)
     {
         $result['_accessory_'] = Link::LINK_TYPE_ACCESSORY;
         return $result;
